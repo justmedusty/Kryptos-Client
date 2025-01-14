df0a9a2c6547ab65f0859b8471cd4605a56b31d6
Dustyn <dustyn_gibb@hotmail.com>
2025-01-14 17:41:13 -0500

    some issues with leaving and rejoining many times need to investigate but we're doing much better now

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::process::exit;
 use std::sync::{Arc, RwLock};
-use std::thread::spawn;
+use std::thread::{sleep, spawn};
+use std::time::Duration;
 use std::{env, io};
 
 static ERROR: i32 = 1;
@@ -12,14 +13,14 @@ type LockedStream = Arc<RwLock<TcpStream>>;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if(args.len() > 3){
+    if (args.len() > 3) {
         println!("Too many arguments!");
         println!("Usage : telnet server_ip port");
         println!("Try --help for help.");
         exit(ERROR);
     }
 
-    if  {args.len() < 2 }{
+    if { args.len() < 2 } {
         println!("Usage : telnet server_ip port");
         println!("Try --help for help.");
         exit(ERROR);
@@ -27,7 +28,7 @@ fn main() {
     if (args[1] == "--help") {
         println!("Usage : telnet server_ip port");
         println!("This is a very simple telnet server client written in Rust.");
-        println!("There is a multiplexing multithreaded server to pair on my github if would like to play around with it.");
+        println!("There is a multithreaded server to pair on my github if would like to play around with it.");
         println!("Options: --help, --version");
         exit(SUCCESS);
     }
@@ -37,7 +38,7 @@ fn main() {
         exit(SUCCESS);
     }
 
-    if  {args.len() < 3 }{
+    if { args.len() < 3 } {
         println!("Usage : telnet server_ip port");
         println!("Try --help for help.");
         exit(ERROR);
@@ -68,6 +69,7 @@ fn main() {
 fn client_read_routine(tcp_stream: LockedStream) {
     loop {
         let mut buffer = vec![0; 1024];
+        sleep(Duration::from_millis(25));
         {
             let mut stream = match tcp_stream.write() {
                 Ok(x) => x,
@@ -83,36 +85,30 @@ fn client_read_routine(tcp_stream: LockedStream) {
                     exit(ERROR);
                 }
             };
+
             match stream.read(&mut buffer) {
-                Ok(0) => continue,
-                Ok(n) => {
-                    buffer.truncate(n);
+                Ok(0) => {
+                    continue;
+                }
+                Ok(_n) => {
                     let data = String::from_utf8_lossy(&buffer);
-                    println!("Received: {}", data);
+                    if data.len() > 0 {
+                        print!("{}", data);
+                    }
+                    for mut byte in buffer {
+                        byte = 0;
+                    }
                 }
                 Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
                 Err(_) => exit(ERROR),
             };
-            match io::stdout().flush() {
-                Ok(x) => x,
-                Err(_) => {
-                    println!("Failed to flush stdout");
-                    exit(ERROR);
-                }
-            };
         }
+
+        io::stdout().flush().unwrap();
     }
 }
 fn client_input_routine(stream: LockedStream) {
     loop {
-        match io::stdout().flush() {
-            Ok(x) => x,
-            Err(_) => {
-                println!("Failed to flush stdout");
-                exit(ERROR);
-            }
-        };
-
         let mut line = String::new();
 
         match io::stdin().read_line(&mut line) {
@@ -128,19 +124,25 @@ fn client_input_routine(stream: LockedStream) {
             continue;
         }
 
-        let mut stream = match stream.write() {
-            Ok(x) => x,
-            Err(_) => {
-                println!("Acquiring write lock on stream failed");
-                exit(ERROR);
-            }
-        };
-        match stream.write_all(line.as_bytes()) {
-            Ok(x) => x,
-            Err(_) => {
-                println!("Failed to write line to stream");
-                exit(ERROR);
-            }
-        };
+        {
+            let mut stream = match stream.write() {
+                Ok(x) => x,
+                Err(_) => {
+                    println!("Acquiring write lock on stream failed");
+                    exit(ERROR);
+                }
+            };
+
+
+            match stream.write_all(line.as_bytes()) {
+                Ok(x) => x,
+                Err(_) => {
+                    println!("Failed to write line to stream");
+                    exit(ERROR);
+                }
+            };
+        }
+
+        sleep(Duration::from_millis(25));
     }
 }
