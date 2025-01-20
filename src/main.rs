274e0635b985e8310ba8922daf9684726d12540e
Dustyn <dustyn_gibb@hotmail.com>
2025-01-20 17:06:06 -0500

    implementing encryption

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+mod rc4;
+
+use crate::rc4::rc4::{Rc4State, KEY_SIZE_BYTES};
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::process::exit;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Mutex, RwLock};
 use std::thread::{sleep, spawn};
 use std::time::Duration;
 use std::{env, io};
@@ -9,24 +12,25 @@ use std::{env, io};
 static ERROR: i32 = 1;
 static SUCCESS: i32 = 0;
 type LockedStream = Arc<RwLock<TcpStream>>;
+type Rc4StateMachine = Arc<Mutex<Rc4State>>;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if (args.len() > 3) {
+    if (args.len() > 4) {
         println!("Too many arguments!");
-        println!("Usage : telnet server_ip port");
+        println!("Usage : telnet server_ip port session_key");
         println!("Try --help for help.");
         exit(ERROR);
     }
 
-    if { args.len() < 2 } {
-        println!("Usage : telnet server_ip port");
+    if { args.len() < 3 } {
+        println!("Usage : telnet server_ip port session_key");
         println!("Try --help for help.");
         exit(ERROR);
     }
     if (args[1] == "--help") {
-        println!("Usage : telnet server_ip port");
+        println!("Usage : telnet server_ip port session_key");
         println!("This is a very simple telnet server client written in Rust.");
         println!("There is a multithreaded server to pair on my github if would like to play around with it.");
         println!("Options: --help, --version");
@@ -39,14 +43,21 @@ fn main() {
     }
 
     if { args.len() < 3 } {
-        println!("Usage : telnet server_ip port");
+        println!("Usage : telnet server_ip port session_key");
         println!("Try --help for help.");
         exit(ERROR);
     }
 
     let ip = &args[1];
     let port = &args[2];
+    let session_key = args[3].clone();
 
+    if session_key.len() != KEY_SIZE_BYTES {
+        eprintln!("Invalid session key! Must be of length {KEY_SIZE_BYTES}. ASk the server administrator for the session key.");
+        exit(ERROR);
+    }
+
+    let rc4 = Arc::new(Mutex::new(Rc4State::new()));
     let result = TcpStream::connect(format!("{}:{}", ip, port));
 
     if (result.is_ok()) {
@@ -59,14 +70,15 @@ fn main() {
         };
         let wrapped_stream = Arc::new(RwLock::new(stream));
         let read_reference = Arc::clone(&wrapped_stream);
+        let rc4_clone = rc4.clone();
         spawn(move || {
-            client_read_routine(Arc::clone(&wrapped_stream));
+            client_read_routine(Arc::clone(&wrapped_stream), rc4_clone);
         });
 
-        client_input_routine(read_reference);
+        client_input_routine(read_reference, rc4);
     }
 }
-fn client_read_routine(tcp_stream: LockedStream) {
+fn client_read_routine(tcp_stream: LockedStream, rc4: Rc4StateMachine) {
     loop {
         let mut buffer = vec![0; 1024];
         sleep(Duration::from_millis(25));
@@ -91,7 +103,15 @@ fn client_read_routine(tcp_stream: LockedStream) {
                     continue;
                 }
                 Ok(_n) => {
-                    let data = String::from_utf8_lossy(&buffer);
+                    let mut decrypted_buffer = buffer.clone();
+                    {
+                        let mut rc4_stream = match rc4.lock() {
+                            Ok(x) => x,
+                            Err(_) => continue,
+                        };
+                        rc4_stream.decrypt(&buffer, &mut decrypted_buffer);
+                    }
+                    let data = String::from_utf8_lossy(&decrypted_buffer);
                     if data.len() > 0 {
                         print!("{}", data);
                     }
@@ -107,7 +127,7 @@ fn client_read_routine(tcp_stream: LockedStream) {
         io::stdout().flush().unwrap();
     }
 }
-fn client_input_routine(stream: LockedStream) {
+fn client_input_routine(stream: LockedStream, rc4: Rc4StateMachine) {
     loop {
         let mut line = String::new();
 
@@ -123,7 +143,13 @@ fn client_input_routine(stream: LockedStream) {
         if line.is_empty() {
             continue;
         }
+        let mut encrypted_buffer = vec![0; 4096];
+        encrypted_buffer.truncate(line.len());
 
+        {
+            let mut rc4_unlocked = rc4.lock().unwrap();
+            rc4_unlocked.encrypt(line.as_bytes(),&mut encrypted_buffer);
+        }
         {
             let mut stream = match stream.write() {
                 Ok(x) => x,
@@ -132,7 +158,6 @@ fn client_input_routine(stream: LockedStream) {
                     exit(ERROR);
                 }
             };
-
 
             match stream.write_all(line.as_bytes()) {
                 Ok(x) => x,
---
+++ src/rc4.rs
@@ -0,0 +1,124 @@
+pub mod rc4 {
+    use rand::RngCore;
+    pub const KEY_SIZE_BYTES: usize = 256;
+
+    #[derive(Debug)]
+    pub struct Rc4State {
+        s: [u8; KEY_SIZE_BYTES],
+        i: usize,
+        j: usize,
+        key: Rc4Key,
+    }
+
+    #[derive(Debug)]
+    pub struct Rc4Key {
+        key: [u8; KEY_SIZE_BYTES],
+    }
+
+    impl Rc4Key {
+        fn new(key: [u8; KEY_SIZE_BYTES]) -> Rc4Key {
+            Rc4Key { key }
+        }
+    }
+
+    impl Rc4State {
+        /// Creates a new Rc4State object with a randomly generated key and default values for the s array, i, j
+        pub fn new() -> Self {
+            let mut new = Self {
+                s: [0; KEY_SIZE_BYTES],
+                i: 0,
+                j: 0,
+                key: Rc4Key::new([0; KEY_SIZE_BYTES]), // Initialize with a default key
+            };
+
+            new.initialize();
+            new
+        }
+
+        pub fn initialize(&mut self) {
+            self.generate_key();
+        }
+
+        /// Generates a key for your Rc4State object, this is called automatically on invocation of ::new however you can call it again if you wish to regenerate a new key
+        /// The key is of size 256 bytes (4096 bits)
+        pub fn generate_key(&mut self) {
+            let mut key = [0u8; KEY_SIZE_BYTES];
+            rand::rng().fill_bytes(&mut key); // Fixed to use a random generator
+            self.key = Rc4Key::new(key);
+        }
+
+        /// key_scheduling sets up the S array (initial key stream) with initial values getting ready to begin the encryption process.
+        fn key_scheduling(&mut self) {
+            let key = &self.key.key;
+
+            // Initialize the s array to the range [0..255]
+            for i in 0..KEY_SIZE_BYTES {
+                self.s[i] = i as u8;
+            }
+
+            let mut j = 0;
+
+            for i in 0..KEY_SIZE_BYTES {
+                j = (j + self.s[i] as usize + key[i] as usize) % KEY_SIZE_BYTES;
+                self.s.swap(i, j);
+            }
+
+            self.i = 0;
+            self.j = 0;
+        }
+
+        /// prga (pseudo-random generator algorithm) sets up the final keystream buffer with pseudo random bytes derived from the initial keystream generated in key_scheduling
+        fn prga(&mut self, output_buffer: &mut [u8]) {
+            self.key_scheduling();
+            for byte in output_buffer {
+                self.i = (self.i + 1) % KEY_SIZE_BYTES;
+                self.j = (self.j + self.s[self.i] as usize) % KEY_SIZE_BYTES;
+                self.s.swap(self.i, self.j);
+                /*
+                   We add some extra mixing, whether this actually adds anything tangible to our result probably not but why not. It's not like this is a serious application of an RC4 variant
+                */
+                self.i =
+                    (self.j * self.s[(self.i * self.j) % KEY_SIZE_BYTES] as usize) % KEY_SIZE_BYTES;
+                self.j =
+                    (self.i * self.s[(self.i * self.j) % KEY_SIZE_BYTES] as usize) % KEY_SIZE_BYTES;
+
+                self.s.swap(self.i, self.j);
+
+                let k =
+                    self.s[(self.s[self.i] as usize + self.s[self.j] as usize) % KEY_SIZE_BYTES];
+                *byte = k;
+            }
+        }
+
+        pub fn set_key(&mut self, key: &[u8]) {
+            if key.len() != KEY_SIZE_BYTES {
+                println!("Key size is not correct! Expected {KEY_SIZE_BYTES}");
+                return;
+            }
+
+            self.key.key = match key.try_into() {
+                Ok(x) => x,
+                Err(_) => self.key.key,
+            };
+        }
+
+        pub fn encrypt(&mut self, input: &[u8], output: &mut [u8]) {
+            let mut keystream = vec![0u8; input.len()];
+
+            if output.len() < input.len() {
+                println!("RC4 encrypt: output buffer too short");
+                return;
+            }
+
+            self.prga(&mut keystream);
+
+            for (i, &input_byte) in input.iter().enumerate() {
+                output[i] = keystream[i] ^ input_byte;
+            }
+        }
+
+        pub fn decrypt(&mut self, input: &[u8], output: &mut [u8]) {
+            self.encrypt(input, output);
+        }
+    }
+}
